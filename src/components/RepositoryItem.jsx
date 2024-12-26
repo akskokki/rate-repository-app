c4f8c3af555e9c68624995dfe59285ab2ce03888
akskokki <akseli.kokki@gmail.com>
2024-12-26 16:09:02 +0200

    Initial commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import { Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <Text>
+      Full name: {item.fullName} {'\n'}
+      Description: {item.description} {'\n'}
+      Language: {item.language} {'\n'}
+      Stars: {item.starGazerscount} {'\n'}
+      Forks: {item.forksCount} {'\n'}
+      Reviews: {item.reviewCount} {'\n'}
+      Rating: {item.ratingAverage}
+    </Text>
+  );
+};
+
+export default RepositoryItem;
