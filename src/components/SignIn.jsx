de80698e5013f899e40312be8dca0b2c4da3c34b
akskokki <akseli.kokki@gmail.com>
2024-12-28 14:33:14 +0200

    Add support for getting access token from server by signing in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,6 +3,7 @@ import Text from './Text';
 import { Pressable, StyleSheet, TextInput, View } from 'react-native';
 import theme from '../theme';
 import * as yup from 'yup';
+import useSignIn from '../hooks/useSignIn';
 
 const initialValues = {
   username: '',
@@ -93,8 +94,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return <SignInForm onSubmit={onSubmit} />;
